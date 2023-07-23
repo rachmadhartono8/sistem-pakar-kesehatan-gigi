3bffed45155eebc37c3ab4a4ff09877da9205def
rachmadhartono8 <rahmadhartono62@gmail.com>
2023-07-23 19:52:25 +0700

    update fungsi php

--- fungsi.php
+++ fungsi.php
@@ -7,16 +7,10 @@ function question($kode){
         echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='question.php?kode=G02'>Ya</a>";
         echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='question.php?kode=G04'>Tidak</a>";
     }
+
     if($kode=='G02'){
         echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='question.php?kode=G03'>Ya</a>";
-        // solusi
         echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='solusi.php?kode=P01'>Tidak</a>";
-    }
-
-    if($kode=='G03'){
-        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='solusi.php?kode=S203'>Ya</a>";
-        // ya akan merefer ke Periodontitis
-        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='question.php?kode='>Tidak</a>";
     }
 
     if($kode=='G04'){
@@ -24,14 +18,32 @@ function question($kode){
         echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='question.php?kode=G10'>Tidak</a>";
     }
 
+    if($kode=='G03'){
+        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='solusi.php?kode=P02'>Ya</a>";
+        // ya akan merefer ke Periodontitis
+        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='question.php?kode=G05'>Tidak</a>";
+    }
+
+    if($kode=='G05'){
+        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='question.php?kode=G12'>Ya</a>";
+        // echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='question.php?kode=G16'>Tidak</a>";
+    }
+    if($kode=='G12'){
+        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='solusi.php?kode=P05'>Ya</a>";
+        // Stomatitis Aftosa
+        // echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='question.php?kode=n8'>Tidak</a>";
+    }
+
+    
+
     if($kode=='G14'){
-        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='solusi.php?kode=S204'>Ya</a>";
+        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='solusi.php?kode=P03'>Ya</a>";
         // Stomatitis Aftosa
         echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='question.php?kode=n8'>Tidak</a>";
     }
 
     if($kode=='G10'){
-        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='solusi.php?kode=S205'>Ya</a>";
+        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='solusi.php?kode=P04'>Ya</a>";
         // Cheilitis
         echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='question.php?kode=G06'>Tidak</a>";
     }
@@ -60,7 +72,7 @@ function question($kode){
 
 
     // Solusi
-    if($kode=='m9-a'){
+    if($kode=='P06'){
         echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='solusi.php?kode=s5'>Ya</a>";
         echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='solusi.php?kode=s6'>Tidak</a>";
     }
@@ -85,40 +97,73 @@ function question($kode){
 
 function kesimpulan($penyakit){
     include 'koneksi.php';
-    $sql = "SELECT * from tb_kesimpulan_gigi WHERE solusi='$penyakit' AND status='setuju'";
+    $sql = "SELECT * from tb_kesimpulan_gigi_new WHERE solusi='$penyakit' AND status='setuju'";
     $data = mysqli_query($connect,$sql);
     while ($row = mysqli_fetch_assoc($data)) {
         echo '<p>-'.$row['fakta'].'</p>';
     }  
 }
 
+// function solusi($kode){    
+//     if ($kode=='S200') {
+//         $penyakit = "Scaling";
+//         kesimpulan($penyakit);        
+//     }
+//     if ($kode=='S203') {
+//         $penyakit = "Root Planing";
+//         kesimpulan($penyakit);        
+//     }
+//     if ($kode=='S204') {
+//         $penyakit = "Medikasi";
+//         kesimpulan($penyakit);
+//     }
+//     if ($kode=='S205') {
+//         $penyakit = "Medikasi";
+//         kesimpulan($penyakit);
+//     }
+//     if ($kode=='S206') {
+//         $penyakit = "Medikasi";
+//         kesimpulan($penyakit);
+//     }
+//     if ($kode=='S207') {
+//         $penyakit = "Medikasi";
+//         kesimpulan($penyakit);
+//     }
+//     if ($kode=='S208') {
+//         $penyakit = "Medikasi & Perawatan Saluran Akar";
+//         kesimpulan($penyakit);
+//     }
+// }
+
+
+
 function solusi($kode){    
-    if ($kode=='S200') {
-        $penyakit = "Scaling";
+    if ($kode=='P01') {
+        $penyakit = "Gingvitis";
         kesimpulan($penyakit);        
     }
-    if ($kode=='S203') {
-        $penyakit = "Root Planing";
+    if ($kode=='P02') {
+        $penyakit = "Periodontitis";
         kesimpulan($penyakit);        
     }
-    if ($kode=='S204') {
-        $penyakit = "Medikasi";
+    if ($kode=='P03') {
+        $penyakit = "Stomatitis Aftosa";
         kesimpulan($penyakit);
     }
-    if ($kode=='S205') {
-        $penyakit = "Medikasi";
+    if ($kode=='P04') {
+        $penyakit = "Halitosis";
         kesimpulan($penyakit);
     }
-    if ($kode=='S206') {
-        $penyakit = "Medikasi";
+    if ($kode=='P05') {
+        $penyakit = "Cheilitis";
         kesimpulan($penyakit);
     }
-    if ($kode=='S207') {
-        $penyakit = "Medikasi";
+    if ($kode=='P06') {
+        $penyakit = "Abses Periodontal";
         kesimpulan($penyakit);
     }
-    if ($kode=='S208') {
-        $penyakit = "Medikasi & Perawatan Saluran Akar";
+    if ($kode=='P07') {
+        $penyakit = "Karies Gigi";
         kesimpulan($penyakit);
     }
 }
--- hapus.php
+++ hapus.php
@@ -1,7 +1,7 @@
 <?php
   include("koneksi.php");
 
-  $query = "DELETE FROM tb_kesimpulan_gigi where kode_kesimpulan=" . $_GET['id'];
+  $query = "DELETE FROM tb_kesimpulan_gigi_new where kode_kesimpulan=" . $_GET['id'];
   if (mysqli_query($connect,$query)){
     header("location:pakar-masukan.php");
   }
