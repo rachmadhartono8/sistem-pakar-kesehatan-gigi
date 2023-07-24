bd5364038919e860a9ad21ec4894e55ee7744f1f
rachmadhartono8 <rahmadhartono62@gmail.com>
2023-07-24 10:40:51 +0700

    kesimpulan_gigi

--- terima.php
+++ terima.php
@@ -1,7 +1,7 @@
 <?php
   include("koneksi.php");
 
-  $query = "UPDATE tb_kesimpulan set status='setuju' where kode_kesimpulan=".$_GET['id']; //diterima bos !
+  $query = "UPDATE tb_kesimpulan_gigi set status='setuju' where kode_kesimpulan=".$_GET['id']; //diterima bos !
   if (mysqli_query($connect,$query)){
     header("location:pakar-masukan.php");
   }
